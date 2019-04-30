8e36dd31a372ef1543d5f21318736f6b4af609bb
Elliot Patros <empatros@gmail.com>
2019-04-30 14:39:40 -0700

    layout with numpad layer

--- keyboards/keebio/iris/keymaps/emp/safe keymap.c
+++ keyboards/keebio/iris/keymaps/emp/safe keymap.c
@@ -7,8 +7,9 @@ extern keymap_config_t keymap_config;
 
 // Layer definitions
 #define _COLEMAK 0
-#define _ALTERNATE 1
-#define _SYMBOL 2
+#define _MODIFIERS 1
+#define _ALTERNATE 2
+#define _SYMBOL 3
 
 // Custom keycode definitions
 enum custom_keycodes {
@@ -45,6 +46,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //                                     └────────┴────────┴────────┘└────────┴────────┴────────┘
     ),
 
+    // momentary on when any non-default layer (any except _COLEMAK) is on
+    [_MODIFIERS] = LAYOUT(
+// ┌────────┬────────┬────────┬────────┬────────┬────────┐                  ┌────────┬────────┬────────┬────────┬────────┬────────┐
+    _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
+// ├────────┼────────┼────────┼────────┼────────┼────────┤                  ├────────┼────────┼────────┼────────┼────────┼────────┤
+    _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
+// ├────────┼────────┼────────┼────────┼────────┼────────┤                  ├────────┼────────┼────────┼────────┼────────┼────────┤
+    _______, _______, _______, _______, _______, _______,                    _______, _______, _______, _______, _______, _______,
+// ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+    _______, KC_LCTL, KC_LALT, KC_LGUI, KC_BSPC, _______, _______,  _______, _______, KC_DEL,  KC_RGUI, KC_RALT, KC_RCTL, _______,
+// └────────┴────────┴────────┴────────┼────────┼────────┼────────┘└────────┼────────┴────────┴────────┴────────┴────────┴────────┘
+                                        _______, _______, _______,  _______, _______, _______
+//                                     └────────┴────────┴────────┘└────────┴────────┴────────┘
+    ),
+
     // momentary on when either L_LAYER or R_LAYER is pressed
     [_ALTERNATE] = LAYOUT(
 // ┌────────┬────────┬────────┬────────┬────────┬────────┐                  ┌────────┬────────┬────────┬────────┬────────┬────────┐
@@ -54,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // ├────────┼────────┼────────┼────────┼────────┼────────┤                  ├────────┼────────┼────────┼────────┼────────┼────────┤
     _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_BRMU,                    MUT_MAC, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
 // ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-    _______, KC_LCTL, KC_LALT, KC_LGUI, KC_BSPC, KC_BRMD, _______,  _______, VDN_MAC, KC_DEL,  KC_RGUI, KC_RALT, KC_RCTL, _______,
+    _______, _______, _______, _______, _______, KC_BRMD, _______,  _______, VDN_MAC, _______, _______, _______, _______, _______,
 // └────────┴────────┴────────┴────────┼────────┼────────┼────────┘└────────┼────────┴────────┴────────┴────────┴────────┴────────┘
                                         _______, _______, _______,  _______, _______, _______
 //                                     └────────┴────────┴────────┘└────────┴────────┴────────┘
@@ -65,11 +81,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // ┌────────┬────────┬────────┬────────┬────────┬────────┐                  ┌────────┬────────┬────────┬────────┬────────┬────────┐
     KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, 
 // ├────────┼────────┼────────┼────────┼────────┼────────┤                  ├────────┼────────┼────────┼────────┼────────┼────────┤
-    _______, _______, _______, _______, _______, _______,                    XXXXXXX, KC_P7,   KC_P8,   KC_P9,   XXXXXXX, KC_F12,
+    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F12,
 // ├────────┼────────┼────────┼────────┼────────┼────────┤                  ├────────┼────────┼────────┼────────┼────────┼────────┤
-    _______, _______, _______, _______, _______, XXXXXXX,                    KC_PDOT, KC_P4,   KC_P5,   KC_P6,   KC_PCMM, _______,
+    _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                    KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
 // ├────────┼────────┼────────┼────────┼────────┼────────┼────────┐┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-    _______, _______, _______, _______, _______, XXXXXXX, _______,  _______, KC_P0,   KC_P1,   KC_P2,   KC_P3,   XXXXXXX, _______,
+    _______, _______, _______, _______, _______, KC_ENT,  _______,  _______, KC_SPC,  _______, _______, _______, _______, _______,
 // └────────┴────────┴────────┴────────┼────────┼────────┼────────┘└────────┼────────┴────────┴────────┴────────┴────────┴────────┘
                                         _______, _______, _______,  _______, _______, _______
 //                                     └────────┴────────┴────────┘└────────┴────────┴────────┘
@@ -128,6 +144,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
     case L_LAYER:
         llayer_pressed = pressed;
         if (pressed) {
+            layer_on(_MODIFIERS);
             layer_on(_ALTERNATE);
             if (rlayer_pressed) {
                 layer_on(_SYMBOL);
@@ -144,6 +161,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
                 layer_off(_SYMBOL);
                 if (! rlayer_pressed) {
                     layer_off(_ALTERNATE);
+                    layer_off(_MODIFIERS);
                 }
             } else if (! rlayer_pressed) {
                 layer_off(_ALTERNATE);
@@ -153,6 +171,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
     case R_LAYER:
         rlayer_pressed = pressed;
         if (pressed) {
+            layer_on(_MODIFIERS);
             layer_on(_ALTERNATE);
             if (llayer_pressed) {
                 layer_on(_SYMBOL);
@@ -169,6 +188,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
                 layer_off(_SYMBOL);
                 if (! llayer_pressed) {
                     layer_off(_ALTERNATE);
+                    layer_off(_MODIFIERS);
                 }
             } else if (! llayer_pressed) {
                 layer_off(_ALTERNATE);
@@ -181,12 +201,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
             mt_timer[LST_LPR_ID] = timer_read();
             mt_used = false;
         } else {
-            unregister_code(KC_LSFT);
             if (!mt_used && timer_elapsed(mt_timer[LST_LPR_ID]) < TAPPING_TERM) {
-                tap_code(KC_LBRC);
+                tap_code(KC_9);
             } else {
                 mt_used = false;
             }
+            unregister_code(KC_LSFT);
         }
         return false;   // this keypress was handled
     case CTL_ESC:       // mod tap: left control
@@ -209,21 +229,23 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
             mt_timer[RST_RPR_ID] = timer_read();
             mt_used = false;
         } else {
-            unregister_code(KC_RSFT);
             if (!mt_used && timer_elapsed(mt_timer[RST_RPR_ID]) < TAPPING_TERM) {
-                tap_code(KC_RBRC);
+                tap_code(KC_0);
             } else {
                 mt_used = false;
             }
+            unregister_code(KC_RSFT);
         }
         return false;   // this keypress was handled
     case S_LAYER:
         slayer_pressed = pressed;
         if (pressed) {
+            layer_on(_MODIFIERS);
             layer_on(_SYMBOL);
         } else {
             if (! (llayer_pressed || rlayer_pressed)) {
                 layer_off(_SYMBOL);
+                layer_off(_MODIFIERS);
             } else if (! (llayer_pressed && rlayer_pressed)) {
                 layer_off(_SYMBOL);
             }
